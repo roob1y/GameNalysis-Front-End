6bc03159dbe04b5cb71f0155e6bea50cf7d66c07
roob1y <robwork122@gmail.com>
2022-11-03 10:18:34 +0000

    fixed height of cards for correct responsive behaviour. Added button to go back to main page from user  review

--- src/components/UserReview.jsx
+++ src/components/UserReview.jsx
@@ -1,6 +1,8 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getUserReview } from "../utils/api";
 import { useState, useEffect } from "react";
+import { BsChevronLeft } from "react-icons/bs";
+
 
 import UserCardReview from "./UserCardReview";
 
@@ -16,10 +18,17 @@ const UserReview = () => {
   }, [reviewId]);
   if (!isLoading) {
     return (
-      <main className="main">
-        <h2>Reviewed By {userReview.owner}</h2>
-        <UserCardReview userReview={userReview} />
-      </main>
+      <>
+        <nav>
+          <Link to="/">
+            <BsChevronLeft title="Home Button" size='2em'/>
+          </Link>
+        </nav>
+        <main className="main">
+          <h2>Reviewed By {userReview.owner}</h2>
+          <UserCardReview userReview={userReview} />
+        </main>
+      </>
     );
   } else {
     return <p>is loading...</p>;
