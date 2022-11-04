73464899c04593f9e9635eb8368e915a3b026bc8
roob1y <robwork122@gmail.com>
2022-11-04 11:46:39 +0000

    comment card populated with data fetched from api. Slight styling with css

--- src/components/CardComments.jsx
+++ src/components/CardComments.jsx
@@ -1,8 +1,11 @@
 const CardComments = ({comments}) => {
   console.log(comments);
   return (
-    <li >
+    <li className="userCommentCard">
+      <h3>{comments.author}</h3>
       <p>{comments.body}</p>
+      <p>votes: {comments.votes}</p>
+      <p>posted: {new Date(comments.created_at).toLocaleDateString()}</p>
     </li>
   )
 }
