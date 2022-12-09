dd438e5beb63b6867c157990f71c68347a7471ca
roob1y <robwork122@gmail.com>
2022-12-09 12:27:53 +0000

    added assets folder with 404 imgs

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import cryingSnake from "../assets/sad_snake.png"
+import cryingLadder from "../assets/sad_ladder.png"
+
+
 
 const PageNotFound = () => {
   return (
     <div>
+      <img src={cryingSnake} alt="Crying Snake"/>
+      <img src={cryingLadder} alt="Crying Snake"/>
+
       <h1>Hello</h1>
     </div>
   );
